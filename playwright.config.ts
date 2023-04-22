4737f675420b4bdb29de98573a4d8d41033d7959
Andres Baron <130755302+andresbaron89@users.noreply.github.com>
2023-04-22 08:10:01 -0500

    prueba de registro
    Prueba de registro ejecutada

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,17 +35,8 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    }
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
     /* Test against mobile viewports. */
     // {
